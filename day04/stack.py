6ef3dfa1cb5967ea6c5a3662c53a43b7549ea9c6
32-Sinha <unacadmya@gmail.com>
2024-11-04 21:29:54 +0600

    day04 done

---
+++ day04/stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+# Example usage
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())  # Output: 3
+print(stack.peek())  # Output: 2
+print(stack.size())  # Output: 2
